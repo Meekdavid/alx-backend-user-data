282f1b7b188355273dc8e405fda7fb035942cc55
David Mboko <98537263+Meekdavid@users.noreply.github.com>
2023-03-16 06:56:59 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,7 +4,7 @@ Encrypting passwords
 """
 import bcrypt
 
-efficient effic
+efficient effi
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string """
     encoded = password.encode()
